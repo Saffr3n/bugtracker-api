a6f3c54abc3e0432b7cd09ca3664cbf483dce15e
Saffr3n <saffr3n@gmail.com>
2024-04-20 19:04:51 +0300

    build: Add mongoose and express-session

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import session from 'express-session';
+import mongoose from 'mongoose';
+import MongoStore from 'connect-mongo';
 import logger from 'morgan';
 import path from 'path';
 import { InternalServerError, PathNotFoundError } from './utils/errors';
@@ -7,11 +10,30 @@ const app = express();
 
 const env = process.env.NODE_ENV || 'development';
 const port = process.env.PORT || 3000;
+const secret = process.env.SECRET || 'secret';
+const dbUri = process.env.DB_URI || 'mongodb://127.0.0.1:27017';
+const dbName = process.env.DB_NAME || 'bugtracker';
+
+mongoose.set('strictQuery', false);
+mongoose.connect(dbUri, { dbName }).catch(console.error);
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    resave: false,
+    saveUninitialized: false,
+    unset: 'destroy',
+    store: new MongoStore({
+      client: mongoose.connection.getClient(),
+      dbName
+    })
+  })
+);
 
 app.use((req, res, next) => next(new PathNotFoundError()));
 app.use((err: Err, req: Req, res: Res, next: Next) => {
