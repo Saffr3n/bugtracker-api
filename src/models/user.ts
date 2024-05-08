ace8247f8c6756a0c893f06cf1f80e966603bf60
Saffr3n <saffr3n@gmail.com>
2024-05-08 19:00:53 +0300

    fix: Improve user model typing

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,13 +1,37 @@
 import mongoose from 'mongoose';
 
-const UserSchema = new mongoose.Schema(
+const userRoles = ['Admin', 'Project Manager', 'Developer', 'User'] as const;
+
+type UserRole = (typeof userRoles)[number];
+
+interface User {
+  readonly username: string;
+  readonly email: string;
+  readonly hash: string;
+  readonly role: UserRole;
+  readonly registerDate: Date;
+  readonly avatarUrl?: string;
+  normalize(): UserNormalized;
+}
+
+interface UserNormalized {
+  readonly id: string;
+  readonly url: string;
+  readonly username: string;
+  readonly email: string;
+  readonly role: UserRole;
+  readonly registerDate: string;
+  readonly avatarUrl?: string;
+}
+
+const UserSchema = new mongoose.Schema<User>(
   {
     username: { type: String, required: true },
     email: { type: String, required: true },
     hash: { type: String, required: true },
     role: {
       type: String,
-      enum: ['Admin', 'Project Manager', 'Developer', 'User'],
+      enum: userRoles,
       default: 'User'
     },
     registerDate: { type: Date, default: new Date() },
@@ -16,7 +40,7 @@ const UserSchema = new mongoose.Schema(
   {
     collection: 'users',
     methods: {
-      normalize() {
+      normalize(): UserNormalized {
         return {
           id: this.id as string,
           url: `/users/${this.username.toLowerCase()}`,
