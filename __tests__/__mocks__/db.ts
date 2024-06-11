a13bf120cc8678973baffb72ea43ef61be49c102
Saffr3n <saffr3n@gmail.com>
2024-06-11 16:40:07 +0300

    fix: Fix default user hash

--- __tests__/__mocks__/db.ts
+++ __tests__/__mocks__/db.ts
@@ -1,7 +1,12 @@
+import bcrypt from 'bcryptjs';
 import User from '../../src/models/user';
 
 export default {
   users: [
-    new User({ username: 'test', email: 'test@example.com', hash: 'Test1234' })
+    new User({
+      username: 'test',
+      email: 'test@example.com',
+      hash: bcrypt.hashSync('Test1234', 12)
+    })
   ]
 };
